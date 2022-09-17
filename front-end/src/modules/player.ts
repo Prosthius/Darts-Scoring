8c113de20e63794ee233395afadaca9cfe58da9f
Prosthius <ietvntpd@duck.com>
2022-09-17 14:49:41 +1000

    Added id attribute to player class

--- front-end/src/modules/player.ts
+++ front-end/src/modules/player.ts
@@ -1,9 +1,11 @@
 export class Player {
     name: string;
     score: number;
+    id: number;
 
-    constructor(_name: string) {
+    constructor(_name: string, _id: number) {
         this.name = _name;
         this.score = 0;
+        this.id = _id;
     }
 }
